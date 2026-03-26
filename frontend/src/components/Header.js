476d5f917421fa0036c526a918093160705c3f36
gergokiss04 <94444225+gergokiss04@users.noreply.github.com>
2026-03-26 09:44:30 +0100

    Simpler header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -15,48 +15,6 @@ const Header = () => {
             <span className="fs-4 fw-bold">Intelligens Útikönyv</span>
           </a>
         </div>
-
-        <div className="col-12 col-md-auto mb-3 mb-md-0 d-flex justify-content-center">
-          <div
-            className="btn-group flex-wrap justify-content-center"
-            role="group"
-            aria-label="Kategóriák"
-          >
-            <button type="button" className="btn btn-outline-light px-3">
-              Összes
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Múzeumok
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Műemlékek
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Szállások
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Éttermek
-            </button>
-          </div>
-        </div>
-
-        <div className="col-12 col-md-3 d-flex justify-content-center justify-content-md-end align-items-center">
-          <div
-            className="btn-group btn-group-sm"
-            role="group"
-            aria-label="Vármegyék"
-          >
-            <button type="button" className="btn btn-outline-light px-3">
-              Borsod
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Heves
-            </button>
-            <button type="button" className="btn btn-outline-light px-3">
-              Nógrád
-            </button>
-          </div>
-        </div>
       </header>
     </div>
   );
